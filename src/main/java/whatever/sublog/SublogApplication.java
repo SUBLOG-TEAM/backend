1b9e064e13ac279aed2f674ced324c5ec90ab9fd
brorica <minjea917@gmail.com>
2023-08-09 01:08:53 +0900

    first commit

---
+++ src/main/java/whatever/sublog/SublogApplication.java
@@ -0,0 +1,13 @@
+package whatever.sublog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SublogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SublogApplication.class, args);
+	}
+
+}
